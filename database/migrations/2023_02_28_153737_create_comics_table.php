a787bc22a3fe71ac9455948599b7a80d1278dccd
GuglielmoBini <guglielmobini@gmail.com>
2023-02-28 18:59:44 +0100

    migrated and seeded

--- database/migrations/2023_02_28_153737_create_comics_table.php
+++ database/migrations/2023_02_28_153737_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->float('price', 6, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50);
+            $table->text('artists');
+            $table->text('writers');
             $table->timestamps();
         });
     }
